176c1da05a3c39b6761d674455782d33a4d4d300
YasaswiniPoluri <109154047+YasaswiniPoluri@users.noreply.github.com>
2024-02-16 15:46:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/',function(req, res){
+
+res.send('Hello World!')
+
+});
+
+app.listen(port, () => {
+
+console.log(`Example app listening on port ${port}`)
+
+})
+
+app.post('/',(req,res)=>{
+    console.log(req.method);
+    res.send('Got a POST request');
+});
+app.put('/',(req,res)=>{
+    console.log(req.method);
+     res.send('got a put request');
+});
+
+
+
+
+
